8cffca9e14028ea5ad58468a769fb794e83ec4c8
stefanorosanelli <stefano.rosanelli@gmail.com>
2018-04-18 19:29:39 +0200

    fix: remove fallback routes

--- config/routes.php
+++ config/routes.php
@@ -1,7 +1,6 @@
 <?php
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
-use Cake\Routing\Route\DashedRoute;
 
 Router::plugin(
     'BEdita/GraphQL',
@@ -14,7 +13,5 @@ Router::plugin(
             '/graphql',
             ['controller' => 'GraphQL', 'action' => 'execute']
         );
-
-        $routes->fallbacks(DashedRoute::class);
     }
 );
